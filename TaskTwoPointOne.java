053c87d7fef7caf3714d09ae56a660dbb1fbdca9
aliali12 <alin.ru@live.com>
2022-02-08 13:31:37 +0300

    MySecondTask

---
+++ TaskTwoPointOne.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TaskTwoPointOne {
+
+    public static void main(String[] args) {
+        System.out.println("Enter your grades");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
+
+        if ( c > max ) {
+            System.out.println("Your minimum Grade is ! " + min);
+            System.out.println("Your maximum Grade is ! " + c);
+        } else if ( c < min) {
+            System.out.println(("Your minimum Grade is ! " + c));
+            System.out.println("Your maximum Grade is ! " +max);
+
+        } else if ( a == b || a ==c ){
+            System.out.println("The Application could not figure minimum and maximum Grades !");
+        }
+    }
+}
+
+
+
+
+
+
+
+
